6715c3b91d35f7dd7863e3253215676aba1d717a
kasukurthi Madhan <kasukurthimadhan@gmail.com>
2024-02-19 11:10:10 +0530

    Added Problem9.js

---
+++ Problem9.js
@@ -0,0 +1,10 @@
+function problem9(input){
+    if(input[0]=='R' || input[0]=='r'){
+        console.log(`${input} Plays banjo`);
+    }
+    else{
+        console.log(`${input} does not play banjo`);
+    }
+}
+
+problem9("avi")
