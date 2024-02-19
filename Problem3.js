559b7a1051f9ab07233b919dc2e5e3d205ab1a49
kasukurthi Madhan <kasukurthimadhan@gmail.com>
2024-02-19 09:04:26 +0530

    Added Problem.js

---
+++ Problem3.js
@@ -0,0 +1,8 @@
+
+function nthEvenNumber(N) {
+    return (N * 2) - 2;
+}
+console.log(nthEvenNumber(1));
+console.log(nthEvenNumber(3));
+console.log(nthEvenNumber(100));
+console.log(nthEvenNumber(1298734));
